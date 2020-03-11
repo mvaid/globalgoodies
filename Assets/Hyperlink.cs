1e4e1d022ca2487c5d772f7a26d8f1c75ba40f12
mvaid <61883149+mvaid@users.noreply.github.com>
2020-03-11 11:08:39 -0500

    Initial Repo Setup

---
+++ Assets/Hyperlink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hyperlink: MonoBehaviour
+{
+    // From the Recipes Page to Balloon Chocolate Bowl
+    public void onClickToScene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
