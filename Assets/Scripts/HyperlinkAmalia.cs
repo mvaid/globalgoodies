223e76e9952ba226ab62e6600e2589641924d4ec
amaliabollero <61883134+amaliabollero@users.noreply.github.com>
2020-03-13 10:42:01 -0500

    Input logo

---
+++ Assets/Scripts/HyperlinkAmalia.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HyperlinkAmalia : MonoBehaviour
+{
+    //about us to chocolate bowl
+public void onClicktoScene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
